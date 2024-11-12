8f9b7cd2a73971fc6ca5fd5f31fd0923b125fea3
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-11-12 09:18:46 +0200

    task 2

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import request from Flask
+
+
+class Auth:
+    def __init__(self):
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require_auth method
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization_header method
+        """
+        return None
+
+    def def current_user(self, request=None) -> TypeVar('User'):
+        """current_user method
+        """
+        return None
