92994b5344e01d4dbf373ce18f0c627f93c798b4
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-11-12 11:15:44 +0200

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,24 @@ class BasicAuth(Auth):
     """Basic Auth class
     """
     pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extracts base64 string
+            Return None if authorization_header is None
+
+            Return None if authorization_header is not a string
+
+            Return None if authorization_header doesn’t start by Basic
+            (with a space at the end)
+
+            Otherwise, return the value after Basic (after the space)
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if authorization_header.startswith("Basic "):
+            return authorization_header[6:]
+        else:
+            return None
