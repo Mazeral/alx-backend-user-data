2dda8f015a652d0804f11efa5ffc7bed4a490545
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-11-13 15:10:58 +0200

    task 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,10 +15,10 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth_type = getenv('AUTH_TYPE', 'auth')
-if auth_type == 'Auth':
+auth_type = os.getenv('AUTH_TYPE', 'Auth')
+if auth_type == 'auth':
     auth = Auth()
-if auth_type == 'BasicAuth':
+if auth_type == 'basicauth':
     auth = BasicAuth()
 
 
