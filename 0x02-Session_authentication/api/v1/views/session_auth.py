9a16176baeb11296a815130a61aae3c175c767d6
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-11-14 18:13:26 +0200

    task 7

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -13,19 +13,19 @@ def login():
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    if email is None or email == "":
+    if email is None or email == '':
         return jsonify({"error": "email missing"}), 400
-    if password is None or password == "":
+    if password is None or password == '':
         return jsonify({"error": "password missing"}), 400
-    users = User.search(email)
-    if users is None:
+    users = User.search({"email": email})
+    if not users or users == []:
         return jsonify({"error": "no user found for this email"}), 404
-    user = users[0]
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-    from api.v1.auth import auth
-    response = jsonify(user.to_json())
-    session_name = getenv('SESSION_NAME')
-    response.set_cookie(session_name, session_id)
-
-    return response
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            resp = jsonify(user.to_json())
+            session_name = os.getenv('SESSION_NAME')
+            resp.set_cookie(session_name, session_id)
+            return resp
+    return jsonify({"error": "wrong password"}), 401
