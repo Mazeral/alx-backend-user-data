41799b214e5b1b3dc7c23105a5c0cc253096aebe
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-11-18 08:48:13 +0200

    task 0

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+This module defines the SQLAlchemy ORM model for the User table.
+It handles user-related data including email, hashed password,
+session ID, and reset token.
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    SQLAlchemy ORM model for the users table.
+
+    Attributes:
+        id (int): The primary key of the user.
+        email (str): The user's email address.
+        hashed_password (str): The user's hashed password.
+        session_id (str | None): The session ID for the user
+        (nullable).
+
+        reset_token (str | None): The token used for resetting
+        the password (nullable).
+    """
+    __tablename__ = 'users'
+
+    id: int = Column(Integer, primary_key=True)
+    email: str = Column(String(250), nullable=False)
+    hashed_password: str = Column(String(250), nullable=False)
+    session_id: str | None = Column(String(250))  # Default nullable=True
+    reset_token: str | None = Column(String(250))  # Default nullable=True
